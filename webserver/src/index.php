a0764a4fcc7232a90fb2d04b41d49c3857b7b43a
root <root@ip-172-31-47-123.ap-south-1.compute.internal>
2019-05-29 02:47:19 +0000

    Updated application code

--- webserver/src/index.php
+++ webserver/src/index.php
@@ -11,8 +11,15 @@ if ($conn->connect_error) {
 }
 $sql = "CREATE TABLE IF NOT EXISTS mytable ( firstname VARCHAR(30) NOT NULL )";
 if ($conn->query($sql) == TRUE) {
-	echo "Table Created Successfully";
+	echo "Table Created Successfully\n";
 } else {
 	echo "Error Creating table: ";
+}
+$sql = "INSERT INTO mytable(firstname) VALUES('Hello')";
+$conn->query($sql);
+$result = $conn->query("SELECT * FROM mytable");
+while($row = $result->fetch_assoc())
+{
+	echo $row['firstname']. "<br>";
 }
 $conn->close();
